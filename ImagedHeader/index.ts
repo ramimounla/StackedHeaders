f4f302faa28a6ffb7e0a4758f603bfdaf4a01d42
Rami Mounla <Rami_Mounla@hotmail.com>
2019-12-19 15:29:32 +1300

    added image visualisation

--- ImagedHeader/index.ts
+++ ImagedHeader/index.ts
@@ -21,8 +21,27 @@ export class ImagedHeader implements ComponentFramework.StandardControl<IInputs,
         // Add control initialization code
         var divHeader: HTMLDivElement;
         divHeader = document.createElement("div");
-        divHeader.innerText = context.parameters.headerText.raw + "";
+		
+		divHeader.style.font = "'SegoeUI-Bold', 'SegoeUI-Semibold', 'Segoe UI Semibold', 'Segoe UI Regular', 'Segoe UI'";
+		divHeader.style.color = context.parameters.fontColor.raw || "";
+		divHeader.style.backgroundColor = context.parameters.backgroundColor.raw || "";
+		divHeader.style.fontSize =  context.parameters.fontSize.raw || "";
+		divHeader.style.backgroundImage = "";
 
+		let imageSpan: HTMLSpanElement;
+		let textSpan: HTMLSpanElement;
+		imageSpan = document.createElement("span");
+		textSpan = document.createElement("span");
+
+		textSpan.innerText = context.parameters.headerText.raw + "";
+		
+		let image: HTMLImageElement;
+		image = document.createElement("img");
+		image.src = context.parameters.image.raw || "";
+
+		imageSpan.appendChild(image);
+		divHeader.appendChild(imageSpan);
+		divHeader.appendChild(textSpan);
         container.appendChild(divHeader);
     }
 
